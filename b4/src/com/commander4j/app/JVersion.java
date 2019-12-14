a4c7589fcc13df3d7813d4b6a9ffa2241437a643
David Garratt <dave@MacBook-Pro-2019.local>
2019-12-14 15:22:34 +0000

    JasperReports 6.11.0

--- b4/src/com/commander4j/app/JVersion.java
+++ b4/src/com/commander4j/app/JVersion.java
@@ -31,7 +31,7 @@ public class JVersion
 {
 
 	public static String getProgramVersion() {
-		return "7.26";
+		return "7.27";
 	}
 
 	public static Double getProgramVersionValue() {
--- b4Middleware/src/com/commander4j/mw/StartMain.java
+++ b4Middleware/src/com/commander4j/mw/StartMain.java
@@ -16,7 +16,7 @@ public class StartMain
 
 	Logger logger = org.apache.logging.log4j.LogManager.getLogger((StartMain.class));
 	public MiddlewareConfig cfg;
-	public static String version = "1.85";
+	public static String version = "1.86";
 	Boolean running = false;
 	LogArchiveThread archiveLog;
 	StatusThread statusthread;
