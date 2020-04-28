0ab3e3347d700982759d0331a78bd624a35be775
David Garratt <dave@MacBook-Pro-2019.local>
2020-04-28 16:56:09 +0100

    Update

--- b4/src/com/commander4j/app/JVersion.java
+++ b4/src/com/commander4j/app/JVersion.java
@@ -31,7 +31,7 @@ public class JVersion
 {
 
 	public static String getProgramVersion() {
-		return "7.50";
+		return "8.00";
 	}
 
 	public static Double getProgramVersionValue() {
@@ -39,7 +39,7 @@ public class JVersion
 	}
 
 	public static int getSchemaVersion() {
-		return 146;
+		return 150;
 	}
 
 }
