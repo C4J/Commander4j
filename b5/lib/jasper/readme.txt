2b47b7cdcdb308b7352ecb34e8e8edbb8f5c8640
David Garratt <support@commander4j.com>
2020-11-30 13:14:14 +0000

    JasperReports updated to 6.16

--- b5/lib/jasper/readme.txt
+++
@@ -1,3 +0,0 @@
-jasperreports-6.8.0
-jasperreports-fonts-6.8.0
-jasperreports-javaflow-6.8.0
--- b5/src/com/commander4j/app/JVersion.java
+++ b5/src/com/commander4j/app/JVersion.java
@@ -31,7 +31,7 @@ public class JVersion
 {
 
 	public static String getProgramVersion() {
-		return "8.08";
+		return "8.09";
 	}
 
 	public static Double getProgramVersionValue() {
