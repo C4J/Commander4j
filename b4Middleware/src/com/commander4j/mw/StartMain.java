55221752aa1025814610a748727146565e2c298f
David Garratt <support@commander4j.com>
2020-12-30 20:00:58 +0000

    Updated jdbc jars

--- b4Middleware/src/com/commander4j/mw/StartMain.java
+++ b4Middleware/src/com/commander4j/mw/StartMain.java
@@ -16,7 +16,7 @@ public class StartMain
 
 	Logger logger = org.apache.logging.log4j.LogManager.getLogger((StartMain.class));
 	public MiddlewareConfig cfg;
-	public static String version = "2.04";
+	public static String version = "2.05";
 	Boolean running = false;
 	LogArchiveThread archiveLog;
 	StatusThread statusthread;
