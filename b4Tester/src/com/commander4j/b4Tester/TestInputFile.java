8b732a54f40896d168410dd64ea393996beec6a2
David Garratt <C4J@users.noreply.github.com>
2021-03-27 17:18:49 +0000

    Delete b4Tester directory

--- b4Tester/src/com/commander4j/b4Tester/TestInputFile.java
+++
@@ -1,9 +0,0 @@
-package com.commander4j.b4Tester;
-
-public class TestInputFile
-{
-	public String id = "";
-	public String inputFilename = "";
-	public String sampleFilename = "";
-
-}
--- b4Tester/src/com/commander4j/b4Tester/TestOutputFile.java
+++
@@ -1,8 +0,0 @@
-package com.commander4j.b4Tester;
-
-public class TestOutputFile
-{
-	public String id = "";
-	public String outputFilename = "";
-	public String referenceFilename = "";
-}
