17203484025b1cc55c3812498a9f648b93934d86
David Garratt <support@commander4j.com>
2024-06-08 06:47:59 +0100

    Update

--- b6/src/com/commander4j/sys/JWindowSplash.java
+++ b6/src/com/commander4j/sys/JWindowSplash.java
@@ -1,41 +1,12 @@
 
 package com.commander4j.sys;
 
-/**
- * @author David Garratt
- * 
- * Project Name : Commander4j
- * 
- * Filename     : JWindowSplash.java
- * 
- * Package Name : com.commander4j.sys
- * 
- * License      : GNU General Public License
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the 
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public 
- * License along with this program.  If not, see
- * http://www.commander4j.com/website/license.html.
- * 
- */
-
-import java.awt.AWTException;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
-import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 
@@ -69,15 +40,8 @@ public class JWindowSplash extends JWindow
 		setBounds(rect);
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		
-		try
-		{
-			bufImage = new Robot().createScreenCapture(rect);
-		}
-		catch (AWTException e)
-		{
-			e.printStackTrace();
-		}
+
+		bufImage =  new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_3BYTE_BGR);
 
 		Graphics2D g2D = bufImage.createGraphics();
 		g2D.drawImage(image, 0, 0, this);
